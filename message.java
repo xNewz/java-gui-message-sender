6b5e9168d8b74b91d92f81a5904a1467016482aa
xNewz <pargornruasijan@gmail.com>
2021-11-27 23:25:59 +0700

    Add libraly

---
+++ message.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
